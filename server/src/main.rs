4b734489557ed7a4959af328ce3aa940a6c2dfe0
maxgurewitz <maxthegeek1@gmail.com>
2019-05-24 10:06:26 -0700

    404 page

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,24 +1,32 @@
 #[macro_use]
 extern crate serde_derive;
 extern crate actix_web;
-use actix_web::{http, server, App, Error, HttpRequest, Json, Responder};
+use actix_web::{http, server, App, Error, HttpRequest, HttpResponse, Json};
 
 #[derive(Serialize)]
 struct HomePageResource {
     name: String,
 }
 
-fn homepageRoute(req: HttpRequest) -> Result<Json<HomePageResource>, Error> {
+fn home_page_route(_req: HttpRequest) -> Result<Json<HomePageResource>, Error> {
     Ok(Json(HomePageResource {
         name: "foo".to_string(),
     }))
 }
 
+fn not_found_page_route(_req: HttpRequest) -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::NotFound()
+        .content_type("application/json")
+        .body("{ \"message\": \"route not found\" }"))
+}
+
 fn main() {
     server::new(|| {
-        App::new().resource("/api/home", |r| {
-            r.method(http::Method::POST).with(homepageRoute)
-        })
+        App::new()
+            .resource("/api/home", |r| {
+                r.method(http::Method::POST).with(home_page_route)
+            })
+            .default_resource(|r| r.with(not_found_page_route))
     })
     .bind("127.0.0.1:8000")
     .expect("Can not bind to port 8000")
