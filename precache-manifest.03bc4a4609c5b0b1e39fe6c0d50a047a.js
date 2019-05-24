4aec9fbc325d8526c8cc4da746e3ac256ebde616
maxgurewitz <maxthegeek1@gmail.com>
2019-05-23 21:03:01 -0700

    Updates

---
+++ precache-manifest.03bc4a4609c5b0b1e39fe6c0d50a047a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b81e4de709683a235d257f6801bc66e",
+    "url": "/qv/index.html"
+  },
+  {
+    "revision": "59ceb13c0c81c1634c5f",
+    "url": "/qv/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/qv/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "59ceb13c0c81c1634c5f",
+    "url": "/qv/static/js/main.2c45778e.chunk.js"
+  },
+  {
+    "revision": "64ea3e8846d175755450",
+    "url": "/qv/static/js/runtime~main.e3d7275a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/qv/static/media/logo.5d5d9eef.svg"
+  }
+]);
