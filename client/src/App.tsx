1eda2a35422cb4381481c70120c5a2e217fd78d8
maxgurewitz <maxthegeek1@gmail.com>
2019-05-26 01:26:25 -0700

    centered login button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Home from './Home';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <CssBaseline />
       <Router>
         <Route exact path="/" component={Home} />
       </Router>
--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import styles from './Home.module.css';
 
 const Home: React.FC = () => {
     return (
-        <div className="Home">
-            Login
-        </div>
+        <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            className={styles.grid}>
+            <div className="Home">
+                <Button variant="contained" color="primary" >
+                    Login
+                </Button>
+            </div>
+        </Grid>
     );
 }
 
