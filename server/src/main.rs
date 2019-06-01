f1c20469abaa334a9e5e0cdcaab9ecd657f7a22a
maxgurewitz <maxthegeek1@gmail.com>
2019-06-01 00:39:42 -0700

    try severely reducing connection pool size

--- server/src/main.rs
+++ server/src/main.rs
@@ -27,7 +27,7 @@ fn build_pg_pool() -> PgPool {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pg_manager = ConnectionManager::<PgConnection>::new(database_url);
-    Pool::builder().build(pg_manager).unwrap()
+    Pool::builder().max_size(2).build(pg_manager).unwrap()
 }
 
 // routes
