308a27ff739bf340f73412ecae0e9dcf3927e513
Maxwell Gurewitz <maxthegeek1@gmail.com>
2019-06-18 21:57:13 -0700

    first attempt at middleware fix compiling

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,104 +1,110 @@
 #[macro_use]
 extern crate serde_derive;
 #[macro_use]
-extern crate diesel;
 extern crate actix_web;
+extern crate diesel;
 
 mod db;
 mod middleware;
 mod models;
 
 use self::models::*;
+use actix_cors::Cors;
+
 use actix_web::error::ErrorInternalServerError;
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
-use reqwest;
 use std::env;
 use std::sync::Arc;
 
 #[derive(Serialize)]
 struct HomePageResource {
-    name: String,
+  user: Arc<Auth0Profile>,
 }
 
 static USER_NOT_FOUND_MSG: &str = "{ \"message\": \"Unable to locate user.\" }";
 
 fn get_user_from_req(req: HttpRequest) -> Result<Arc<Auth0Profile>, Error> {
-    let extensions = req.extensions();
+  let extensions = req.extensions();
 
-    extensions
-        .get::<middleware::AuthExtension>()
-        .map(|e| e.user.clone())
-        .ok_or_else(|| ErrorInternalServerError(USER_NOT_FOUND_MSG))
+  extensions
+    .get::<middleware::AuthExtension>()
+    .map(|e| e.user.clone())
+    .ok_or_else(|| ErrorInternalServerError(USER_NOT_FOUND_MSG))
 }
 
 // routes
-fn home_page_route(req: HttpRequest) -> Result<web::Json<HomePageResource>, Error> {
-    let _user = get_user_from_req(req)?;
+fn user_info_route(req: HttpRequest) -> Result<web::Json<HomePageResource>, Error> {
+  let user = get_user_from_req(req)?;
 
-    Ok(web::Json(HomePageResource {
-        name: "foo".to_string(),
-    }))
+  Ok(web::Json(HomePageResource { user: user }))
 }
 
 fn not_found_page_route(_req: HttpRequest) -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::NotFound()
-        .content_type("application/json")
-        .body("{ \"message\": \"route not found\" }"))
+  Ok(
+    HttpResponse::NotFound()
+      .content_type("application/json")
+      .body("{ \"message\": \"route not found\" }"),
+  )
 }
 
 fn login_route(
-    _data: web::Data<middleware::AppData>,
-    _reg: HttpRequest,
+  _data: web::Data<middleware::AppData>,
+  _reg: HttpRequest,
 ) -> Result<HttpResponse, Error> {
-    // create user if not found
-    Ok(HttpResponse::NotFound()
-        .content_type("application/json")
-        .body("{ \"message\": \"success\" }"))
+  // create user if not found
+  Ok(
+    HttpResponse::Ok()
+      .content_type("application/json")
+      .body("{ \"message\": \"success\" }"),
+  )
 }
 
 // auth flow https://auth0.com/docs/flows/concepts/auth-code
 // https://auth0.com/docs/flows/guides/implicit/add-login-implicit
 // https://auth0.com/docs/architecture-scenarios/spa-api
 fn main() {
-    let port = env::var("PORT").unwrap_or("8000".to_string());
+  let port = env::var("PORT").unwrap_or("8000".to_string());
 
-    println!("Attempting to bind to port: {}", port);
+  println!("Attempting to bind to port: {}", port);
 
-    HttpServer::new(|| {
-        let pg_pool = db::build_pg_pool();
+  HttpServer::new(|| {
+    let pg_pool = db::build_pg_pool();
 
-        let env: middleware::Env = env::var("ENV")
-            .map(|s| {
-                if s == "dev" {
-                    middleware::Env::Dev
-                } else {
-                    middleware::Env::Prod
-                }
-            })
-            .unwrap_or(middleware::Env::Prod);
+    let env: middleware::Env = env::var("ENV")
+      .map(|s| {
+        if s == "dev" {
+          middleware::Env::Dev
+        } else {
+          middleware::Env::Prod
+        }
+      })
+      .unwrap_or(middleware::Env::Prod);
 
-        let http_client = reqwest::Client::new();
+    let http_client = actix_web::client::Client::default();
 
-        App::new()
-            .data(middleware::AppData {
-                pg_pool: pg_pool.clone(),
-                _env: env,
-                http_client,
-            })
-            .wrap(actix_web::middleware::Logger::default())
-            .service(
-                web::scope("/api")
-                    .service(
-                        web::scope("/private/")
-                            .wrap(middleware::Auth)
-                            .route("/home", web::post().to(home_page_route)),
-                    )
-                    .route("/login", web::post().to(login_route)),
-            )
-            .default_service(web::route().to(not_found_page_route))
-    })
-    .bind(format!("0.0.0.0:{}", port))
-    .expect("Can not bind to port")
-    .run()
-    .unwrap();
+    // FIXME Access to XMLHttpRequest at 'localhost:8000/api/private/user_info' from origin 'http://localhost:3000' has been blocked by CORS policy: Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, https.
+    // https://docs.rs/actix-cors/0.1.0/actix_cors/
+    App::new()
+      .wrap(Cors::new().send_wildcard())
+      .data(middleware::AppData {
+        pg_pool: pg_pool.clone(),
+        _env: env,
+        http_client,
+      })
+      .wrap(actix_web::middleware::Logger::default())
+      .service(
+        web::scope("/api")
+          .service(
+            web::scope("/private/")
+              .wrap(middleware::Auth)
+              .route("/user_info", web::post().to(user_info_route)),
+          )
+          .route("/login", web::post().to(login_route)),
+      )
+      .default_service(web::route().to(not_found_page_route))
+  })
+  .bind(format!("0.0.0.0:{}", port))
+  .expect("Can not bind to port")
+  .run()
+  .unwrap();
 }
--- server/src/middleware.rs
+++ server/src/middleware.rs
@@ -1,133 +1,176 @@
-extern crate reqwest;
 use super::db;
 use super::models::Auth0Profile;
 use actix_service::{Service, Transform};
 use actix_web::dev::{ServiceRequest, ServiceResponse};
-use actix_web::error::{ErrorUnauthorized, ErrorInternalServerError};
+use actix_web::error::{ErrorInternalServerError, ErrorUnauthorized};
 use actix_web::http::header::AUTHORIZATION;
 use actix_web::{Error, HttpMessage};
-use futures::future::{err, ok, Either, FutureResult};
-use futures::Poll;
-use std::sync::Arc;
+use actix_utils::cloneable::CloneableService;
+use actix_web::web::BytesMut;
+use futures::{Future, Stream};
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Claims {
-    sub: String,
-    email: String,
-    email_verified: bool,
-}
+use std::collections::HashMap;
+
+use futures::future::{FutureResult, ok};
+use futures::Poll;
+
+use std::sync::Arc;
 
 static USER_INFO_URL: &str = "https://maxthegeek1.auth0.com/userinfo";
 
-fn request_user_info(bearer_token: &str, app_data: &AppData) -> Result<Auth0Profile, reqwest::Error> {
-    let mut response = app_data
-        .http_client
+#[derive(Debug, Deserialize)]
+struct HttpBinResponse {
+    args: HashMap<String, String>,
+    data: String,
+    files: HashMap<String, String>,
+    form: HashMap<String, String>,
+    headers: HashMap<String, String>,
+    json: Auth0Profile,
+    origin: String,
+    url: String,
+}
+
+// Implemented from example: 
+// https://github.com/actix/examples/blob/aa5750cafc9d6bc96e098c338b70fa618149ffd5/async_ex1/src/main.rs
+fn fetch_user_info(
+    client: &actix_web::client::Client,
+    bearer_token: &String,
+) -> impl Future<Item = Auth0Profile, Error = Error> {
+    client
         .get(&USER_INFO_URL.to_string())
         .header(
-            reqwest::header::AUTHORIZATION,
-            format!("Bearer {}", bearer_token),
+          "Authorization",
+          format!("Bearer {}", bearer_token),
         )
-        .send()?;
-
-    response.json()
+        .send()
+        .map_err(|e| {
+          println!("Unable to parse bearer token: {}", e);
+          ErrorUnauthorized(INVALID_TOKEN_MSG)
+        })
+        .and_then(|resp| {
+            resp.from_err()
+                .fold(BytesMut::new(), |mut acc, chunk| {
+                    acc.extend_from_slice(&chunk);
+                    Ok::<_, Error>(acc)
+                })
+                .map(|body| {
+                    let body: HttpBinResponse = serde_json::from_slice(&body).unwrap();
+                    body.json
+                })
+        })
 }
 
 pub struct Auth;
 
 impl<S, B> Transform<S> for Auth
 where
-    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>
+        + 'static,
     S::Future: 'static,
+    B: 'static,
 {
-    type Request = ServiceRequest;
-    type Response = ServiceResponse<B>;
-    type Error = Error;
-    type InitError = ();
-    type Transform = AuthMiddleware<S>;
-    type Future = FutureResult<Self::Transform, Self::InitError>;
+  type Request = ServiceRequest;
+  type Response = ServiceResponse<B>;
+  type Error = Error;
+  type InitError = ();
+  type Transform = AuthMiddleware<S>;
+  type Future = FutureResult<Self::Transform, Self::InitError>;
 
-    fn new_transform(&self, service: S) -> Self::Future {
-        ok(AuthMiddleware { service })
-    }
+  fn new_transform(&self, service: S) -> Self::Future {
+    ok(AuthMiddleware { service: CloneableService::new(service) })
+  }
 }
 
 pub struct AuthMiddleware<S> {
-    service: S,
+  service: CloneableService<S>,
 }
 
 pub struct AuthExtension {
-    pub user: Arc<Auth0Profile>,
+  pub user: Arc<Auth0Profile>,
 }
 
 fn parse_bearer(req: &ServiceRequest) -> Option<String> {
-    let header = req.headers().get(AUTHORIZATION)?;
-    // "Bearer *" length
-    if header.len() < 8 {
-        return Option::None;
-    }
+  let header = req.headers().get(AUTHORIZATION)?;
+  // "Bearer *" length
+  if header.len() < 8 {
+    return Option::None;
+  }
 
-    let mut parts = header.to_str().ok()?.splitn(2, ' ');
+  let mut parts = header.to_str().ok()?.splitn(2, ' ');
 
-    match parts.next() {
-        Some(scheme) if scheme == "Bearer" => (),
-        _ => return Option::None,
-    }
+  match parts.next() {
+    Some(scheme) if scheme == "Bearer" => (),
+    _ => return Option::None,
+  }
 
-    let token = parts.next()?;
+  let token = parts.next()?;
 
-    Option::from(token.to_string())
+  Option::from(token.to_string())
 }
 
 static MISSING_HEADER_MSG: &str = "{ \"message\": \"Must pass Authorization Bearer header.\" }";
 static INVALID_TOKEN_MSG: &str = "{ \"message\": \"Invalid Bearer header.\" }";
 static INTERNAL_SERVICE_ERROR_MSG: &str = "{ \"message\": \"Oops something went wrong.\" }";
 
-fn get_user_info(req: &ServiceRequest) -> Result<Auth0Profile, actix_web::Error> {
-    let bearer = parse_bearer(&req).ok_or(ErrorUnauthorized(MISSING_HEADER_MSG))?;
+fn get_user_info_args(req: &ServiceRequest) -> Result<(String, actix_web::client::Client), actix_web::Error> {
+  let bearer = parse_bearer(&req).ok_or(ErrorUnauthorized(MISSING_HEADER_MSG))?;
 
-    let app_data = req
-        .app_data::<AppData>()
-        .ok_or_else(|| ErrorInternalServerError(INTERNAL_SERVICE_ERROR_MSG))?;
+  let app_data = req
+    .app_data::<AppData>()
+    .ok_or(ErrorInternalServerError(INTERNAL_SERVICE_ERROR_MSG))?;
 
-    request_user_info(&bearer, &app_data).map_err(|_| ErrorUnauthorized(INVALID_TOKEN_MSG))
+  Result::Ok((bearer, app_data.http_client.clone()))
+}
+
+fn fetch_user_info_from_req(req: &ServiceRequest) -> impl Future<Item = Auth0Profile, Error = Error> {
+  let user_info = get_user_info_args(&req);
+  FutureResult::from(user_info)
+  .and_then(|(bearer, client)|
+    fetch_user_info(&client, &bearer)
+  )
 }
 
 impl<S, B> Service for AuthMiddleware<S>
 where
-    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
-    S::Future: 'static,
+  S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
+  S::Future: 'static,
+  B: 'static,
 {
-    type Request = ServiceRequest;
-    type Response = ServiceResponse<B>;
-    type Error = Error;
-    type Future = Either<S::Future, FutureResult<Self::Response, Self::Error>>;
+  type Request = ServiceRequest;
+  type Response = ServiceResponse<B>;
+  type Error = Error;
+  type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
 
-    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
-        self.service.poll_ready()
-    }
+  fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+    self.service.poll_ready()
+  }
 
-    fn call(&mut self, req: ServiceRequest) -> Self::Future {
-        get_user_info(&req)
-          .map(|user_info| {
-            let extension = AuthExtension {
-                user: Arc::new(user_info),
-            };
+  fn call(&mut self, req: ServiceRequest) -> Self::Future {
+      let user_info_fut = fetch_user_info_from_req(&req);
 
-            req.extensions_mut().insert(extension);
-            Either::A(self.service.call(req))
-          })
-          .unwrap_or_else(|http_err| Either::B(err(http_err)))
-    }
+      // modeled after example in actix-redix 
+      let mut srv = self.service.clone();
+
+      Box::new(user_info_fut
+        .and_then(move |user_info| {
+          let extension = AuthExtension {
+            user: Arc::new(user_info),
+          };
+
+          req.extensions_mut().insert(extension);
+          srv.call(req)
+        }))
+  }
 }
 
 pub struct AppData {
-    pub pg_pool: db::PgPool,
-    pub _env: Env,
-    pub http_client: reqwest::Client,
+  pub pg_pool: db::PgPool,
+  pub _env: Env,
+  pub http_client: actix_web::client::Client
 }
 
 #[derive(Clone, Copy)]
 pub enum Env {
-    Dev,
-    Prod,
+  Dev,
+  Prod,
 }
